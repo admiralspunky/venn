4adc96933600a55a1466f8e515c1da6d460698dd
admiralspunky <67512649+admiralspunky@users.noreply.github.com>
2025-07-27 14:21:20 -0400

    Update rules.js

--- rules.js
+++ rules.js
@@ -323,7 +323,7 @@ const allPossibleRules = [
       "bag", "box", "radio", "baby", "knife", "bottle", "sword", "backpack", "helmet", "goggles", "mirror", "alarm", "pillow", "torch", "bucket", "hat",
       "broom", "plate", "mask", "towel", "rope", "bookbag", "phone", "key", "wallet", "pen", "tablet", "notebook", "flashlight", "camera", "thermos", "scarf",
       "snack", "dice", "lantern", "toy", "umbrella", "shovel", "candle", "calculator", "ball", "brick", "booklet", "cassette", "doll", "leash", "paintbrush",
-      "comb", "brush", "staff", 'helmet','pencil'
+      "comb", "brush", "staff", 'helmet','pencil','boots','ruler'
     ]
   },
   {
